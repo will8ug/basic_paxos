9f8bbd791731a8579fbc97335da7fd9163cc9625
Will <yzj_000@163.com>
2024-08-18 16:42:25 +0800

    A basic run in main()

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,32 @@ mod acceptor;
 mod proposal;
 mod proposer;
 
-// use acceptor::Acceptor;
-// use proposer::Proposer;
-// use std::sync::{Arc, Mutex};
+#[double]
+use acceptor::Acceptor;
+use mockall_double::double;
+use proposer::Proposer;
+use std::sync::{Arc, Mutex};
 
 fn main() {
-    println!("Run the tests!");
     test_one_proposer_one_acceptor_no_learner();
 }
 
 fn test_one_proposer_one_acceptor_no_learner() {
-    // let acceptor = Arc::new(Mutex::new(Acceptor::new()));
-    // let acceptors = vec![Arc::clone(&acceptor)];
-    // println!("  ===== Before Start =====");
-    // println!("Acceptors: {:#?}", acceptors);
+    let acceptor = Arc::new(Mutex::new(Acceptor::new()));
+    let acceptors = vec![Arc::clone(&acceptor)];
+    println!("  ===== Before Start =====");
+    println!("Acceptors: {:#?}", acceptors);
 
-    // let mut proposer = Proposer::new(acceptors);
-    // println!("Proposers: {:#?}", proposer);
+    let mut proposer = Proposer::new(acceptors);
+    println!("Proposers: {:#?}", proposer);
 
-    // println!("  ===== Working =====");
-    // let result = proposer.propose(100);
-    // assert_eq!(result, Some(100));
+    println!("  ===== Working =====");
+    let result = proposer.propose(100);
+    assert_eq!(result, Some(100));
 
-    // println!("  ===== After consensus =====");
-    // println!("Proposers: {:#?}", proposer);
-    // println!("Acceptor: {:#?}", acceptor);
+    println!("  ===== After consensus =====");
+    println!("Proposers: {:#?}", proposer);
+    println!("Acceptor: {:#?}", acceptor);
 }
 
 #[cfg(test)]
